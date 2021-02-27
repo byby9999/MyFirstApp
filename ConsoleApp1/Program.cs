cf86f0dddca79c186503640cec8f8539c2a5153a
alexandra <alexandrabalean92@gmail.com>
2021-02-27 11:54:05 +0200

    redo initial stage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int x = 50;
+            Console.WriteLine("Hello world!!!!!");
+
+            Console.ReadKey();
         }
 
     }
